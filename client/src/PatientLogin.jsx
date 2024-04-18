385c121bc8850741a877dad27ec6539147374f7c
amd282 <amd282@yahoo.com>
2024-04-18 10:20:42 -0400

    added patient registaration and login

---
+++ client/src/PatientLogin.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function PatientLogin() {
+  let navigate = useNavigate();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const authenticateUser = async () => {
+    try {
+      const response = await axios.post("http://localhost:3004/loginPatient", {
+        username,
+        password,
+      });
+      const data = response.data;
+      if (data) {
+        navigate("/patient-page");
+      }
+    } catch (error) {
+      alert(`Login failed ${error.message}`);
+      console.error("Error during authentication:", error);
+    }
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">  
+            <div className="bg-white p-4 roundedw-25">
+              <h1>Patient Login</h1>
+
+              <form>
+                
+                <div className="mb-3">
+                  <label htmlFor="username">
+                        <strong>User Name</strong>
+                    </label>
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      placeholder="Enter user name"
+                      className="form-control rounded-0"
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="password">
+                        <strong>Password</strong>
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Enter password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                  <br />
+                  <button type="button" className="btn btn-success w-100 rounded-0" onClick={authenticateUser}>
+                    Login
+                  </button>
+
+                </form>
+
+            </div>
+          </div>
+
+
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default PatientLogin;
---
+++ client/src/PatientRegister.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Container, Form, Button, Spinner } from "react-bootstrap";
+
+function PatientRegister() {
+  let navigate = useNavigate();
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [showLoading, setShowLoading] = useState(false);
+
+  const saveUser = async (e) => {
+    setShowLoading(true);
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3004/registerPatient", {
+        username: user.username,
+        password: user.password,
+      });
+      setShowLoading(false);
+
+      console.log("Results from Register:", response.data);
+
+      navigate("/patientlogin");
+    } catch (error) {
+      setShowLoading(false);
+      console.error(error);
+    }
+  };
+
+  // handles onChange event
+  const onChange = (e) => {
+    e.persist();
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <Container>
+      <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">  
+        <div className="bg-white p-4 roundedw-25">
+          <h1>Patient Registration</h1>
+
+          <form className="form" onSubmit={saveUser}>
+
+          <div className="mb-3">
+            <label htmlFor="username">
+                <strong>User Name</strong>
+            </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Enter user name"
+                value={user.username}
+                onChange={onChange}
+              />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password">
+                <strong>Password</strong>
+            </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter password"
+                value={user.password}
+                onChange={onChange}
+              />
+          </div>
+
+            <br></br>
+            
+            <button type="submit" className="btn btn-success w-100 rounded-0">
+              Register
+            </button>
+
+          </form>
+
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default PatientRegister;
